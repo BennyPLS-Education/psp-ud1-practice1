2986acdeba0c1c50ab4fd2db3d274f9fc7f528dd
Andreu Garcia <andreustreaming@gmail.com>
2023-10-17 17:47:48 +0200

    Activitat 1

---
+++ child_psp_ud1_JAP_AGB_MSV/src/substituirlletra/SubstituirLletra.java
@@ -0,0 +1,14 @@
+package substituirlletra;
+
+import java.util.Scanner;
+
+public class SubstituirLletra {
+    public static void main(String[] args) {
+        var scan = new Scanner(System.in);
+
+        var text_to_replace = scan.nextLine();
+        var text_to_replace_with = scan.nextLine();
+
+        
+    }
+}
