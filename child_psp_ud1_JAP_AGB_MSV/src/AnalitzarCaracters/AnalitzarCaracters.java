26877ceb301c5d4739e08515529b485ba30d47c1
Marc <marcsans26@gmail.com>
2023-10-17 18:20:57 +0200

    activitat 2

--- child_psp_ud1_JAP_AGB_MSV/src/AnalitzarCaracters/AnalitzarCaracters.java
+++ child_psp_ud1_JAP_AGB_MSV/src/AnalitzarCaracters/AnalitzarCaracters.java
@@ -4,31 +4,31 @@ import java.io.*;
 import java.util.Scanner;
 
 public class AnalitzarCaracters {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         var scanner = new Scanner(System.in);
         var letter = scanner.nextLine();
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-
         String linea = null;
-        try {
-            while((linea = in.readLine()) != null) {
-                System.out.println(countLetter(linea, letter.charAt(0)));
-            }
-            in.close();
-        } catch(IOException ex) {
-            System.out.println("Error al mostrar el proces fill");
+
+        /* A l'hora de comptar caracters nomes te en compte el primer caracter
+        * si posem una paraula, nomes contara la primera lletra
+        */
+        while((linea = in.readLine()) != null) {
+            System.out.println(countLetter(linea, letter.charAt(0)));
         }
+
+        in.close();
     }
 
     private static int countLetter(String text, char letter) {
-        int counter = 0;
+        int count = 0;
 
         for (int i = 0; i < text.length(); i++) {
             if(text.charAt(i) == letter) {
-                counter++;
+                count++;
             }
         }
 
-        return counter;
+        return count;
     }
 }
