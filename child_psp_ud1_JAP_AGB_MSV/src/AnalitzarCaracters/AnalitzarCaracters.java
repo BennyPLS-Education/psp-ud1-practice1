82dd9507506d1272251e2e1f0f39ac6d6bf0ad16
Marc <marcsans26@gmail.com>
2023-10-17 17:56:54 +0200

    activitat 2

--- child_psp_ud1_JAP_AGB_MSV/src/AnalitzarCaracters/AnalitzarCaracters.java
+++ child_psp_ud1_JAP_AGB_MSV/src/AnalitzarCaracters/AnalitzarCaracters.java
@@ -1,20 +1,34 @@
 package AnalitzarCaracters;
 
+import java.io.*;
+import java.util.Scanner;
+
 public class AnalitzarCaracters {
-    public static void main(String[] args) {
-        char letter = 0;
-        countLetter(args[0], letter);
+    public static void main(String[] args) throws IOException {
+        var scanner = new Scanner(System.in);
+        var letter = scanner.nextLine();
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+        String linea = null;
+        try {
+            while((linea = in.readLine()) != null) {
+                System.out.println(countLetter(linea, letter.charAt(0)));
+            }
+            in.close();
+        } catch(IOException ex) {
+            System.out.println("Error al mostrar el proces fill");
+        }
     }
 
-    public static int countLetter(String text, char letter){
+    private static int countLetter(String text, char letter) {
         int counter = 0;
-        for (int i = 0; i < text.length(); i++)
-        {
-            if(text.charAt(i) == letter)
-            {
+
+        for (int i = 0; i < text.length(); i++) {
+            if(text.charAt(i) == letter) {
                 counter++;
             }
         }
+
         return counter;
     }
 }
