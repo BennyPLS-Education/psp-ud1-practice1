d2f7fcf4b2f59b26b2ad942a8498997d2c8672a3
Marc <marcsans26@gmail.com>
2023-10-17 18:48:40 +0200

    activitat 2 - pendent de test

--- child_psp_ud1_JAP_AGB_MSV/src/AnalitzarCaracters/AnalitzarCaracters.java
+++ child_psp_ud1_JAP_AGB_MSV/src/AnalitzarCaracters/AnalitzarCaracters.java
@@ -3,19 +3,32 @@ package AnalitzarCaracters;
 import java.io.*;
 import java.util.Scanner;
 
+/*
+* count the number of times a character appears in a html
+*
+* PROTOCOL
+* 1. send old char
+* 2. send html
+* 3. you shuld receive the number of times the char is in the html
+*
+* IMPORTANT
+* when the only char that is being count is the first char,
+* if you send a word it just going to count the first char
+* */
+
 public class AnalitzarCaracters {
     public static void main(String[] args) throws Exception {
         var scanner = new Scanner(System.in);
         var letter = scanner.nextLine();
+
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         String linea = null;
+        String text = "";
 
-        /* A l'hora de comptar caracters nomes te en compte el primer caracter
-        * si posem una paraula, nomes contara la primera lletra
-        */
         while((linea = in.readLine()) != null) {
-            System.out.println(countLetter(linea, letter.charAt(0)));
+            text += linea;
         }
+        System.out.println(countLetter(text, letter.charAt(0)) + "\n");
 
         in.close();
     }
