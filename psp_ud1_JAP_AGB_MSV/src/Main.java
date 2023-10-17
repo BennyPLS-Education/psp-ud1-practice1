37aeef3aa18f96f7ebd140bfba062c2f02ecf770
Benny <peptoni0345@gmail.com>
2023-10-17 16:44:12 +0200

    User-input URL functionality
    The public static void main method calls for the getUrl method, and that method uses a try/catch block.
    
    This allows the program to receive and return a user-inputted URL.
    
    It also protects the program from an IllegalArgumentException.

--- psp_ud1_JAP_AGB_MSV/src/Main.java
+++ psp_ud1_JAP_AGB_MSV/src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
+
+    private static final Scanner console = new Scanner(System.in);
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        getUrl();
+    }
+
+    static URL getUrl() {
+        try {
+            System.out.print("Enter a URL: ");
+
+            return new URL(getUserInput());
+        } catch (IllegalArgumentException e) {
+            System.out.println("ERROR : " + e.getMessage());
+            return getUrl();
+        }
+    }
+
+    static String getUserInput() {
+        return console.nextLine();
     }
 }
