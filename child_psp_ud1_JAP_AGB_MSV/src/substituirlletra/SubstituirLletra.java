49dd8e67d8c72563f46516979b3fb537e3a46d7d
Andreu Garcia <andreustreaming@gmail.com>
2023-10-17 18:19:46 +0200

    Refactor activitat 3

--- child_psp_ud1_JAP_AGB_MSV/src/substituirlletra/SubstituirLletra.java
+++ child_psp_ud1_JAP_AGB_MSV/src/substituirlletra/SubstituirLletra.java
@@ -1,26 +1,17 @@
 package substituirlletra;
 
 import java.io.BufferedReader;
-import java.io.FileWriter;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.List;
 
 public class SubstituirLletra {
     public static void main(String[] args) throws Exception {
-        List<String> lines = new LinkedList<>();
-//        var file = new FileWriter()
+
         var in = new BufferedReader(new InputStreamReader(System.in));
 
         var text_to_replace = in.readLine();
         var text_to_replace_with = in.readLine();
 
-
-        String line;
-        while ((line = in.readLine()) != null)
-            lines.add(line.replaceAll(text_to_replace, text_to_replace_with));
-
-        lines.forEach(System.out::println);
-
+        in.lines().map(line -> line.replaceAll(text_to_replace, text_to_replace_with))
+                  .forEach(System.out::println);
     }
 }
