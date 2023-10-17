38c44b58ca9cdb16c5e49de3fcd6b4a4c092bee9
Andreu Garcia <andreustreaming@gmail.com>
2023-10-17 18:35:53 +0200

    Activitat 3 escriure arxiu

--- child_psp_ud1_JAP_AGB_MSV/src/substituirlletra/SubstituirLletra.java
+++ child_psp_ud1_JAP_AGB_MSV/src/substituirlletra/SubstituirLletra.java
@@ -1,7 +1,10 @@
 package substituirlletra;
 
 import java.io.BufferedReader;
+import java.io.FileWriter;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
 /**
  * Reads two characters, the character to be replaced and the character
  * that replaces. Then we read the text content (html) and print the replaced text
@@ -14,11 +17,14 @@ import java.io.InputStreamReader;
 public class SubstituirLletra {
     public static void main(String[] args) throws Exception {
         var in = new BufferedReader(new InputStreamReader(System.in));
-
+        var writer = new PrintWriter(new FileWriter("encrypted.dat"));
         var text_to_replace = in.readLine();
         var text_to_replace_with = in.readLine();
 
         in.lines().map(line -> line.replaceAll(text_to_replace, text_to_replace_with))
-                  .forEach(System.out::println);
+                  .forEach(writer::println);
+        
+        writer.close();
+        System.out.println("File 'encrypted.dat' created successfully");
     }
 }
