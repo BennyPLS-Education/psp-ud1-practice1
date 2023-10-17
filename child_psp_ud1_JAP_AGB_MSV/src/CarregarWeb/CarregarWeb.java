c8d7fc65f750a48200cf326501682f84bf622e85
Andreu Garcia <andreustreaming@gmail.com>
2023-10-17 17:34:17 +0200

    Activitat 1 funcionant

---
+++ child_psp_ud1_JAP_AGB_MSV/src/CarregarWeb/CarregarWeb.java
@@ -0,0 +1,15 @@
+package CarregarWeb;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class CarregarWeb {
+    public static void main(String[] args) throws IOException {
+        var scanner = new Scanner(System.in);
+        var URL = scanner.nextLine();
+
+        var process = new ProcessBuilder("curl", URL).start();
+        var in = process.getInputStream();
+        in.transferTo(System.out);
+    }
+}
