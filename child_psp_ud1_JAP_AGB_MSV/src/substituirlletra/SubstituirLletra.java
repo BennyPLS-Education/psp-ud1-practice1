352e6d20ea2ab9fdc79b111bf0042b41dad40e63
Andreu Garcia <andreustreaming@gmail.com>
2023-10-17 17:56:39 +0200

    Activitat 3

--- child_psp_ud1_JAP_AGB_MSV/src/substituirlletra/SubstituirLletra.java
+++ child_psp_ud1_JAP_AGB_MSV/src/substituirlletra/SubstituirLletra.java
@@ -1,14 +1,26 @@
 package substituirlletra;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileWriter;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.List;
 
 public class SubstituirLletra {
-    public static void main(String[] args) {
-        var scan = new Scanner(System.in);
+    public static void main(String[] args) throws Exception {
+        List<String> lines = new LinkedList<>();
+//        var file = new FileWriter()
+        var in = new BufferedReader(new InputStreamReader(System.in));
 
-        var text_to_replace = scan.nextLine();
-        var text_to_replace_with = scan.nextLine();
+        var text_to_replace = in.readLine();
+        var text_to_replace_with = in.readLine();
 
-        
+
+        String line;
+        while ((line = in.readLine()) != null)
+            lines.add(line.replaceAll(text_to_replace, text_to_replace_with));
+
+        lines.forEach(System.out::println);
+
     }
 }
